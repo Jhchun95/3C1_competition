13d6ae49c41cbdb011056fd0d6927f02bbc3dee4
Jhchun95 <jjh1059@gmail.com>
2020-11-30 14:34:06 +0900

    01day

---
+++ 01day/src/HelloJava.java
@@ -0,0 +1,26 @@
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+	
+//		System.out.println("안녕 자바");
+		// print : 출력하고 줄을 바꾸지 않음
+		// println : 출력하고 줄을 바꿈
+//	
+//			
+//		int sum = 0;
+//
+//		for(int i=1; i<=100; i++) {
+//			sum += i;
+//		}
+//		System.out.println("sum의 값은 : "+ sum);
+//		
+		int a=1, b, c;
+		
+		b = a++;
+		c = ++a;
+
+		System.out.println("a= " + a + ", b=" + b + ", c=" + c);
+		
+	}
+}
