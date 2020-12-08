021bd7a42faef2290783c92f994ae9b3606bcd4d
Jhchun95 <jjh1059@gmail.com>
2020-12-08 21:48:28 +0900

    0916

---
+++ 0916/src/GoodsStock.java
@@ -0,0 +1,23 @@
+
+public class GoodsStock {
+	String goodsCode;
+	int stockNum;
+	GoodsStock(String code, int num)
+	{
+		goodsCode = code;
+		stockNum = num;
+	}
+
+	void addStock(int amount)
+	{
+		stockNum += amount;
+	}
+
+	int subtractStock(int amount)
+	{
+		if(stockNum < amount)
+			return 0;
+			stockNum -= amount;
+			return amount;
+	}
+}
